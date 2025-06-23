86bf7f662dae51dcd0869d12066031064d3c3b2b
saiteja-in <vurukondasaiteja13@gmail.com>
2025-06-24 04:07:59 +0530

    added middleware

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='text-blue-600 text-6xl text-center flex justify-center min-h-screen font-mono'>
+    <div className='text-blue-600 text-6xl flex items-center justify-center min-h-screen font-mono'>
       auraspeak
     </div>
   )

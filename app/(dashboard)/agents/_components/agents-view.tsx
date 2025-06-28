dc81e2756db1f66c9ce79a462448676b5ae10f6f
saiteja-in <vurukondasaiteja13@gmail.com>
2025-06-28 22:55:20 +0530

    created a responsive dialog

--- app/(dashboard)/agents/_components/agents-view.tsx
+++ app/(dashboard)/agents/_components/agents-view.tsx
@@ -1,13 +1,17 @@
 "use client";
 import { ErrorState } from "@/components/error-state";
 import { Loader } from "@/components/loading-state";
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/lib/trpc/client";
 import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return <div>
+    {JSON.stringify(data, null, 2)}
+    </div>;
 };
 
 export const AgentsViewLoading = () => {

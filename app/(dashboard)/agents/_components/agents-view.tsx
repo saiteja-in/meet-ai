dd4f3780d2b2fab1c064301bfad9fa9e31a85560
saiteja-in <vurukondasaiteja13@gmail.com>
2025-06-28 22:24:19 +0530

    now using server side rendering, using suspense query

--- app/(dashboard)/agents/_components/agents-view.tsx
+++ app/(dashboard)/agents/_components/agents-view.tsx
@@ -2,24 +2,18 @@
 import { ErrorState } from "@/components/error-state";
 import { Loader } from "@/components/loading-state";
 import { useTRPC } from "@/lib/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
-  const { data, isLoading, isError } = useQuery(
-    trpc.agents.getMany.queryOptions()
-  );
-  if (isLoading) {
-    return (
-        <div className="flex justify-center items-center h-full w-full">
-        <Loader />
-      </div>
-    );
-  }
-  if (isError) {
-    return (<div>
-        <ErrorState title="Failed to load agents" description="Something went wrong" />
-    </div>)
-  }
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
   return <div>{JSON.stringify(data, null, 2)}</div>;
+};
+
+export const AgentsViewLoading = () => {
+  return (
+    <div className="flex justify-center items-center h-full w-full">
+      <Loader />
+    </div>
+  );
 };
--- app/(dashboard)/agents/page.tsx
+++ app/(dashboard)/agents/page.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import { AgentsView } from './_components/agents-view'
+import React, { Suspense } from "react";
+import { AgentsView, AgentsViewLoading } from "./_components/agents-view";
+import { getQueryClient, trpc } from "@/lib/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-const page = () => {
+const page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+
   return (
-   <AgentsView/>
-  )
-}
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<AgentsViewLoading/>}>
+      <AgentsView />
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
 
-export default page
+export default page;
